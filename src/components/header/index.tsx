c8c8b5106a55a9189740b54c25afa2bfc501e518
Leonardo <leonardodacunha16@gmail.com>
2023-07-23 16:11:50 -0300

    fix: header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
 import styles from "./header.module.css";
-import logoimg from "../../assets/logo.svg";
+import logo from "../../assets/logo.svg";
+import logoString from "../../assets/logoString.svg";
 
 export function Header() {
   return (
     <header className={styles.container}>
       <div>
+        s
         <Link to="/">
-          <img src={logoimg} alt="Logo Cripto" />
+          <div>
+            <img className={styles.logo} src={logo} alt="Logo Cripto" />
+            <img
+              className={styles.logoString}
+              src={logoString}
+              alt="Logo String"
+            />
+          </div>
         </Link>
       </div>
     </header>
